2b358bea457c9ad27da7b093baa97a12bac70c46
Simon Münker <simon.muenker@googlemail.com>
2025-08-23 11:07:01 +0000

    prepare, big5 negated experiment

---
+++ experiments/big_5/05--with-history-negated/experiment.py
@@ -0,0 +1,36 @@
+import pathlib
+
+import rich
+
+import llm_questionnaires as llmq
+
+EXP_PATH: str = "experiments/big_5/05--with-history-negated"
+
+
+models = [
+    llmq.agent.AgentModel(id="llama3.1:8b"),
+    llmq.agent.AgentModel(id="mistral:7b"),
+    llmq.agent.AgentModel(id="qwen2.5:7b"),
+    llmq.agent.AgentModel(id="llama3.3:70b"),
+    llmq.agent.AgentModel(id="mistral-large:123b"),
+    llmq.agent.AgentModel(id="qwen2.5:72b"),
+]
+rich.print(models)
+
+personas = llmq.agent.AgentPersona.from_directory(pathlib.Path(f"{EXP_PATH}/personas/"))
+rich.print(personas)
+
+questionnaire = llmq.Questionnaire(
+    path=pathlib.Path("data/big_5/questionnaire_negated.json")
+)
+rich.print(questionnaire)
+
+pipeline = llmq.Pipeline(
+    iterations=250,
+    use_memory=True,
+    personas=personas,
+    models=models,
+    questionnaire=questionnaire,
+    experiment_path=pathlib.Path(EXP_PATH),
+)
+pipeline()
